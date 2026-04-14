9766658683c9ae24609740268bcf9c1abab213ec
GiaBaoo <ngbao840@gmail.com>
2026-04-14 17:31:16 +0700

    add Controller for pages link

--- src/main/java/com/tmdt/fashion_shop/controller/PageController.java
+++ src/main/java/com/tmdt/fashion_shop/controller/PageController.java
@@ -1,4 +1,22 @@
 package com.tmdt.fashion_shop.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PageController {
+
+    @GetMapping("/user/profile")
+    public String profilePage() {
+        return "user/profile";
+    }
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "auth/register";
+    }
 }
