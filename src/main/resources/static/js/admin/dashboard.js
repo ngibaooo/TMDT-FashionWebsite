0f6c4fb19154f2ae76e64dc23281bc0b2399534d
GiaBaoo <ngbao840@gmail.com>
2026-04-22 01:47:40 +0700

    update RevenueByWeekChart

--- src/main/resources/static/js/admin/dashboard.js
+++ src/main/resources/static/js/admin/dashboard.js
@@ -328,7 +328,11 @@ function formatMoney(amount) {
 }
 
 function formatDate(date) {
-    return date.toISOString().split("T")[0];
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
 }
 function renderOrderStatus(data) {
     const container = document.getElementById("orderStatus");
